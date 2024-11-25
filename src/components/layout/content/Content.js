d69644c5607f907328312a67d75b7c9bac3efaef
DesenvolvimentoSJ2 <135130880+DesenvolvimentoSJ2@users.noreply.github.com>
2024-11-25 10:16:50 -0300

    feat: criação e listagem das tarefas

--- src/components/layout/content/Content.js
+++ src/components/layout/content/Content.js
@@ -1,31 +1,70 @@
-import HeaderContent from './Header';
-import styles from './style.module.css';
+import HeaderContent from "./Header";
+import styles from "./style.module.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function Content() {
-    return (
-        <div className={styles.content}>
-            <HeaderContent />
-            <div className={styles.main}>
-                <div className={styles.headerMain}>
-                    <div>
-                        <h3>Projetos</h3>
-                        <span>Você tem <span className={styles.textActive}>2</span> projeto</span>
-                        <div className="offcanvas"></div>
-                    </div>
-                    <button className={styles.buttonAdd}>+ Add</button>
-                </div>
+  const [projects, setProjects] = useState([]);
 
-                <div className={styles.mainContent}>
-                    <div className={styles.project}>
-                        <h3>Name project</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut ducimus a ad quaerat fugiat.</p>
-                        <div className={styles.projectInfor}>
-                            <span className={styles.projectInforStatus}>Andamento</span>
-                            <span>10 dias</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  const fetchProjects = async () => {
+    try {
+      const response = await axios.get("http://192.168.1.45:3000/api/projects");
+      setProjects(response.data);
+    } catch (error) {
+      console.error("Erro na requisição:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProjects();
+
+    const intervalId = setInterval(() => {
+      fetchProjects();
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className={styles.content}>
+      <HeaderContent />
+      <div className={styles.main}>
+        <div className={styles.headerMain}>
+          <div>
+            <h3>Projetos</h3>
+            <span>
+              Você tem{" "}
+              <span className={styles.textActive}>{projects.length}</span>{" "}
+              projeto
+            </span>
+            <div className="offcanvas"></div>
+          </div>
+          <button className={styles.buttonAdd}>+ Add</button>
         </div>
-    );
+
+        <div className="flex flex-wrap gap-3">
+          {projects.length == 0 ? (
+            <li>Carregando projetos...</li>
+          ) : (
+            projects.map((project, index) => 
+              <div key={index} className={styles.mainContent}>
+                <div className={styles.project}>
+                  <h3>{project.name}</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+                    ducimus a ad quaerat fugiat.
+                  </p>
+                  <div className={styles.projectInfor}>
+                    <span className={styles.projectInforStatus}>{project.status}</span>
+                    <span>{project.duration} dias</span>
+                  </div>
+                </div>
+              </div>
+            )
+          )}
+        </div>
+
+      </div>
+    </div>
+  );
 }
---
+++ src/pages/projetos/index.js
@@ -0,0 +1,137 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export default function Home() {
+  const [name, setName] = useState("");
+  const [status, setStatus] = useState("aguardando");
+  const [sequence, setSequence] = useState(0);
+  const [duration, setDuration] = useState();
+
+  const [projects, setProjects] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        console.log("Iniciando requisição para a API...");
+        const response = await fetch(
+          "http://192.168.1.45:3000/api/projects",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json; charset=UTF-8",
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error(`Erro: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        setProjects(data);
+        console.log("Projetos recebidos:", data);
+      } catch (error) {
+        setError(error.message);
+        console.error("Erro na requisição:", error);
+      }
+    };
+
+    fetchProjects();
+  }, []);
+
+  async function apiRoute() {
+    const data = {
+      name,
+      status,
+      duration,
+    };
+
+    try {
+      const response = await axios.post(
+        "http://192.168.1.45:3000/api/projects",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log("Resposta do servidor:", response.data);
+      setStatus("Sucesso! Projeto criado.");
+    } catch (error) {
+      console.error("Erro na requisição:", error);
+      setStatus("Erro ao criar projeto.");
+    }
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    apiRoute();
+  };
+
+  return (
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <form onSubmit={handleSubmit} method="POST">
+        <div className="mb-3">
+          <label htmlFor="name">Nome do Projeto</label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Nome do Projeto"
+            className="block w-full rounded-md border-0 py-1.5 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="status">Selecione o Status</label>
+          <select
+            id="status"
+            name="status"
+            value={status}
+            onChange={(e) => setStatus(e.target.value)}
+            className="block w-full rounded-md border-0 py-2 pl-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+          >
+            <option value="aguardando">Aguardando</option>
+            <option value="em desenvolvimento">Em desenvolvimento</option>
+            <option value="concluido">Concluído</option>
+          </select>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="duration">Duração do projeto (dias)</label>
+          <input
+            id="duration"
+            name="duration"
+            type="text"
+            value={duration}
+            onChange={(e) => setDuration(e.target.value)}
+            placeholder="Sequência do projeto"
+            className="block w-full rounded-md border-0 py-1.5 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
+          />
+        </div>
+        <button
+          type="submit"
+          className="mt-4 w-full py-2 px-4 bg-blue-500 text-white rounded-md"
+        >
+          Enviar
+        </button>
+      </form>
+
+      <div className="mt-10">
+        <h1>Projetos</h1>
+        <ul>
+          {projects.length === 0 ? (
+            <li>Carregando projetos...</li>
+          ) : (
+            projects.map((project, index) => (
+              <li key={index}>{project.name}</li>
+            ))
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+}
